d878f7d0d13feb3e4ab91f4e8c2d6b0af6a57c1a
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-05-07 11:45:09 +0530

    Got the motors working

--- Arduino_code/main.ino
+++ Arduino_code/main.ino
@@ -2,6 +2,11 @@
 
 #define I2C_SLAVE_ADDRESS 11
 
+int right_forward_pin = 7;
+int left_forward_pin = 8;
+int right_backward_pin = 4;
+int left_backward_pin = 12;
+
 void receiveEvents(int numBytes)
 {  
   Serial.println(F("---> recieved events"));
@@ -11,8 +16,23 @@ void receiveEvents(int numBytes)
   }
 }
 
+void forward(){
+  digitalWrite(right_forward_pin, HIGH);
+  digitalWrite(left_forward_pin, HIGH);
+  digitalWrite(right_forward_pin, LOW);
+  digitalWrite(left_forward_pin, LOW);
+}
+
 void setup()
 {
+  Serial.println("Initialising the motors!!!");
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(8, OUTPUT);
+
   Wire.begin(I2C_SLAVE_ADDRESS);
   Serial.begin(9600); 
   Serial.println("Start Reading");
@@ -21,5 +41,7 @@ void setup()
 }
 
 void loop(){
-  
+  forward();
 }
+
+
--- RaspberryPi_Code/application.py
+++ RaspberryPi_Code/application.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask.wrappers import Request
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def index():
-    return render_template("index.html")
+    return render_template("index.html", name=request.args.get("name"))
