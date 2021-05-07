99be627e92eb4f20fb0b6276eff16ba3abd6e54b
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-05-07 12:02:19 +0530

    PWM signal can be sent over internet trhough raspberry pi

--- Arduino_code/main.ino
+++ Arduino_code/main.ino
@@ -7,32 +7,46 @@ int left_forward_pin = 8;
 int right_backward_pin = 4;
 int left_backward_pin = 12;
 
-void receiveEvents(int numBytes)
-{  
+int right_forward_pwm = 0;
+int left_forward_pwm = 0;
+int right_backward_pwm = 0;
+int left_backward_pwm = 0;
+
+int pwm_values[5] = {0, 0, 0, 0, 0};
+
+void receiveEvents(int numBytes){  
   Serial.println(F("---> recieved events"));
+  int counter = 0;
   while (Wire.available()){
     int c = Wire.read();
-    Serial.println(c);
+    pwm_values[counter] = c;
+    counter++;
   }
+  right_forward_pwm = pwm_values[1];
+  left_forward_pwm = pwm_values[2];
+  right_backward_pwm = pwm_values[3];
+  left_backward_pwm = pwm_values[4];
+  Serial.println(right_forward_pwm);
+  Serial.println(left_forward_pwm);
+  Serial.println(right_backward_pwm );
+  Serial.println(left_backward_pwm);
 }
 
-void forward(){
-  analogWrite(right_backward_pin, 255);
-  analogWrite(left_backward_pin, 255);
-  analogWrite(right_forward_pin, 0);
-  analogWrite(left_forward_pin, 0);
+void move(){
+  analogWrite(right_forward_pin, right_forward_pwm);
+  analogWrite(left_forward_pin, left_forward_pwm);
+  analogWrite(right_backward_pin, right_backward_pwm);
+  analogWrite(left_backward_pin, left_backward_pwm);
 }
 
-
-void setup()
-{
+void setup(){
   Serial.println("Initialising the motors!!!");
   pinMode(5, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(7, OUTPUT);
+  pinMode(right_backward_pin, OUTPUT);
+  pinMode(right_forward_pin, OUTPUT);
   pinMode(6, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(8, OUTPUT);
+  pinMode(left_backward_pin, OUTPUT);
+  pinMode(left_forward_pin, OUTPUT);
 
   Wire.begin(I2C_SLAVE_ADDRESS);
   Serial.begin(9600); 
@@ -42,7 +56,6 @@ void setup()
 }
 
 void loop(){
-  forward();
+  move();
+  delay(100);
 }
-
-
