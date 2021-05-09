f5e00cd3daf4545b9f40b664f32683d584cc311e
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-05-09 11:25:07 +0530

    Added LED

--- Arduino_code/main.ino
+++ Arduino_code/main.ino
@@ -2,6 +2,9 @@
 
 #define I2C_SLAVE_ADDRESS 11
 
+int led_pin = 19;
+int led_status = LOW;
+
 int right_forward_pin = 7;
 int left_forward_pin = 8;
 int right_backward_pin = 4;
@@ -12,7 +15,7 @@ int left_forward_pwm = 0;
 int right_backward_pwm = 0;
 int left_backward_pwm = 0;
 
-int pwm_values[5] = {0, 0, 0, 0, 0};
+int pwm_values[6] = {0, 0, 0, 0, 0, 0};
 
 void receiveEvents(int numBytes){  
   Serial.println(F("---> recieved events"));
@@ -26,17 +29,20 @@ void receiveEvents(int numBytes){
   left_forward_pwm = pwm_values[2];
   right_backward_pwm = pwm_values[3];
   left_backward_pwm = pwm_values[4];
+  led_status = pwm_values[5];
   Serial.println(right_forward_pwm);
   Serial.println(left_forward_pwm);
   Serial.println(right_backward_pwm );
   Serial.println(left_backward_pwm);
-}
+  Serial.println(led_status);
+} 
 
 void move(){
   analogWrite(right_forward_pin, right_forward_pwm);
   analogWrite(left_forward_pin, left_forward_pwm);
   analogWrite(right_backward_pin, right_backward_pwm);
   analogWrite(left_backward_pin, left_backward_pwm);
+  digitalWrite(led_pin, led_status);
 }
 
 void setup(){
@@ -47,6 +53,7 @@ void setup(){
   pinMode(6, OUTPUT);
   pinMode(left_backward_pin, OUTPUT);
   pinMode(left_forward_pin, OUTPUT);
+  pinMode(led_pin, OUTPUT);
 
   Wire.begin(I2C_SLAVE_ADDRESS);
   Serial.begin(9600); 
