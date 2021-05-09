9b3c5773fd3e1c3a41bde29aea11f2acb1240de9
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-05-09 18:21:54 +0530

    New Branch for remote

--- RaspberryPi_Code/application.py
+++ RaspberryPi_Code/application.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
-from flask.wrappers import Request
 
 app = Flask(__name__)
 
 @app.route("/")
-
 def index():
-    return render_template("index.html", name=request.args.get("name"))
+    return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    return render_template("success.html")
