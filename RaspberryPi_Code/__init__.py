0ff17a0f251bd2843c3700116a76ddab68349c3f
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-05-10 18:26:08 +0530

    Established communication b/w JS and Falsk

--- RaspberryPi_Code/__init__.py
+++ RaspberryPi_Code/__init__.py
@@ -1,5 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
+
+@app.route('/hello', methods=['GET', 'POST'])
+def data():
+
+    # POST request
+    if request.method == 'POST':
+        print('Incoming..')
+        print(request.get_json())  # parse as JSON
+        return 'OK', 200
+
+    # GET request
+    else:
+        message = {'greeting':'Hello from Flask!'}
+        print(jsonify(message))
 
 @app.route('/')
 def hello():
