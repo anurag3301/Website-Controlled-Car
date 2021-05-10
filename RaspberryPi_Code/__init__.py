36531d0b9548a58e5fcc0938bdfd1845066a6b8f
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-05-10 18:37:51 +0530

    Parsed out the json data

--- RaspberryPi_Code/__init__.py
+++ RaspberryPi_Code/__init__.py
@@ -7,7 +7,11 @@ def data():
     # POST request
     if request.method == 'POST':
         print('Incoming..')
-        print(request.get_json())  # parse as JSON
+        data_json=request.get_json()  # parse as JSON
+        joy1X = data_json["joy1X"], data_json["joy1Y"], data_json["joy2X"], data_json["joy2Y"])
+        joy1Y = data_json["joy1Y"]
+        joy2X = data_json["joy2X"]
+        joy2Y = data_json["joy2Y"]
         return 'OK', 200
 
     # GET request
